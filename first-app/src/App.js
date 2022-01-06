187b767bdf0b6bf7a9874e66f48ae3cad74728c9
Abdul Rahaman <89024694+abdulrahaman12@users.noreply.github.com>
2022-01-06 12:13:39 +0530

    Initial commit

---
+++ first-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Restaurant from './Restaurant App/Restaurant';
+
+const App = () => {
+  return (
+    <div>
+      <Restaurant/>
+    </div>
+  )
+}
+
+export default App
